939f068e227dde2b4e796e8f58faca6653cc5a62
Nikita Melnikov <nik.y.melnikov@gmail.com>
2020-11-17 12:26:28 +0000

    Filled login component with components

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,17 +1,64 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
-  root: {},
+  root: {
+    padding: 20,
+    maxWidth: 300,
+    '& > h2': {
+      margin: 0,
+      marginBottom: 20,
+    },
+  },
+  inputs: {
+    width: '100%',
+  },
+  button: {
+    backgroundColor: 'rgb(0,25,168)',
+    '&:hover': {
+      backgroundColor: 'rgb(20,45,188)',
+    },
+    color: 'WhiteSmoke',
+  },
 }));
 
 const Login = () => {
   const classes = useStyles();
 
-  return (
-    <div>
-      Login
-    </div>
+  return (ß
+    <Paper elevation={3} className={classes.root}>
+      <h2>Login</h2>
+      <Box className={classes.inputs} mb={2}>
+        <TextField
+          id="username"
+          label="Username"
+          variant="outlined"
+          fullWidth
+        />
+      </Box>
+      <Box className={classes.inputs} mb={2}>
+        <TextField
+          id="password"
+          label="Password"
+          variant="outlined"
+          fullWidth
+        />
+      </Box>
+      <Box>
+        <Button
+          className={classes.button}
+          variant="contained"
+          size="large"
+          fullWidth
+        >
+          Login
+        </Button>
+      </Box>
+    </Paper>
   );
 };
 
