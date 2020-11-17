5b986ce07b8108ad401bc9099995f223db830d78
Nikita Melnikov <nik.y.melnikov@gmail.com>
2020-11-17 11:56:40 +0000

    Added base of login component

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+}));
+
+const Login = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      Login
+    </div>
+  );
+};
+
+export default Login;
