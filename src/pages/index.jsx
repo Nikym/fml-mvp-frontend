c82fea0cd1dd5821535b9d901b5f63e1ecd8906d
Nikita Melnikov <nik.y.melnikov@gmail.com>
2020-11-17 17:55:08 +0000

    Removed description from index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,15 +14,7 @@ const HomePage = () => {
   return (
     <Layout>
       <div className={classes.root}>
-        <h1>Welcome!</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Sed sagittis porta nisl eu dapibus. Etiam elit felis, ultricies ut lacinia molestie,
-          placerat eu ligula. In consequat nibh ex, sed laoreet lorem elementum eget.
-          Suspendisse faucibus metus in lobortis hendrerit. Morbi sagittis sapien quis
-          nisi aliquam, at volutpat lacus venenatis. Nam eleifend lacus sed nisl faucibus
-          iaculis. Duis porttitor finibus massa, id suscipit ante gravida at.
-        </p>
+        test
       </div>
     </Layout>
   );
