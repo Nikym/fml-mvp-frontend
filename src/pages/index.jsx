311b25f68b4a84742f7aba1ec75b2934a75123f2
Nikita Melnikov <nik.y.melnikov@gmail.com>
2020-11-17 11:56:29 +0000

    Added basic description consisting of lorem ipsum

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import Layout from '../components/layout/Layout';
 
 const useStyles = makeStyles(() => ({
+  root: {
+    textAlign: 'center',
+  },
 }));
 
 const HomePage = () => {
@@ -10,7 +13,17 @@ const HomePage = () => {
 
   return (
     <Layout>
-      Hello world
+      <div className={classes.root}>
+        <h1>Welcome!</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Sed sagittis porta nisl eu dapibus. Etiam elit felis, ultricies ut lacinia molestie,
+          placerat eu ligula. In consequat nibh ex, sed laoreet lorem elementum eget.
+          Suspendisse faucibus metus in lobortis hendrerit. Morbi sagittis sapien quis
+          nisi aliquam, at volutpat lacus venenatis. Nam eleifend lacus sed nisl faucibus
+          iaculis. Duis porttitor finibus massa, id suscipit ante gravida at.
+        </p>
+      </div>
     </Layout>
   );
 };
