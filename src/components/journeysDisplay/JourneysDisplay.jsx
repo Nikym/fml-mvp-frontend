47f6c592be5603dea1788fbb8612f8959f0bacdb
Nikita Melnikov <nik.y.melnikov@gmail.com>
2020-11-18 19:14:09 +0000

    Added Journey mapping to JourneysDisplay

--- src/components/journeysDisplay/JourneysDisplay.jsx
+++ src/components/journeysDisplay/JourneysDisplay.jsx
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
+import Journey from '../journey/Journey';
+
+const mockJourneys = {
+  journeys: [
+    {
+      start: 'SE8 5RU',
+      end: 'WC1A 2SL',
+      routes: [
+        {
+          orderedLines: [
+            {
+              name: 'DLR',
+            },
+            {
+              name: 'Victoria',
+            },
+          ],
+          journeyTime: 35,
+        },
+        {
+          orderedLines: [
+            {
+              name: 'Jubilee',
+            },
+          ],
+          journeyTime: 20,
+        },
+      ],
+      meanJourneyTime: 27.5,
+      includesNoChangeRoute: true,
+    },
+    {
+      start: 'SE8 5RU',
+      end: 'WC1A 2SL',
+      routes: [
+        {
+          orderedLines: [
+            {
+              name: 'DLR',
+            },
+            {
+              name: 'Victoria',
+            },
+          ],
+          journeyTime: 35,
+        },
+      ],
+      meanJourneyTime: 27.5,
+      includesNoChangeRoute: false,
+    },
+  ],
+};
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -31,6 +84,21 @@ const useStyles = makeStyles(() => ({
 
 const JourneysDisplay = () => {
   const classes = useStyles();
+  const [from, setFrom] = useState('');
+  const [to, setTo] = useState('');
+
+  const handleChangeFrom = (event) => {
+    const { value } = event.target;
+    setFrom(value);
+  };
+
+  const handleChangeTo = (event) => {
+    const { value } = event.target;
+    setTo(value);
+  };
+
+  const handleSubmit = () => {
+  };
 
   return (
     <div className={classes.root}>
@@ -43,6 +111,8 @@ const JourneysDisplay = () => {
               id="from"
               label="From"
               helperText="Eg, SE8 5RU"
+              value={from}
+              onChange={handleChangeFrom}
               // size="small"
               fullWidth
             />
@@ -55,6 +125,8 @@ const JourneysDisplay = () => {
               id="to"
               label="To"
               helperText="Eg, WC1A 2SL"
+              value={to}
+              onChange={handleChangeTo}
               // size="small"
               fullWidth
             />
@@ -68,12 +140,14 @@ const JourneysDisplay = () => {
               size="large"
               className={classes.button}
               fullWidth
+              disabled={!from || !to}
             >
               Search
             </Button>
           </Box>
         </Grid>
       </Grid>
+      {mockJourneys.journeys.map((journey) => <Journey journey={journey} />)}
     </div>
   );
 };
